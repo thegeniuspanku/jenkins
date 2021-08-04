c78f1c0844f96334ee9dd49d1a49abe16eb9edee
thegeniuspanku <thegeniuspanku@gmail.com>
2021-08-04 18:06:08 +0530

    java file changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld{
 	System.out.println("Hello World !!");
 	System.out.println("Welcome to Jenkins World !!");
 	System.out.println("Welcome to Jenkins World using Git Pull..........!!");
+	System.out.println("GIT JENKINS INTEGRATION SUCCESSFUL..............!!");
+
 
 	}
 
